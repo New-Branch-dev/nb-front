9fa49274d280ed2419faba207032ce054533cbda
ctsiwonnim <siwonnima@gmail.com>
2026-06-07 19:04:43 +0900

    사용하지 않는 router삭제

--- src/features/auth/model/UseLogin.ts
+++ src/features/auth/model/UseLogin.ts
@@ -1,8 +1,6 @@
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export const useLogin = () => {
-  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = async (loginId: string, password: string) => {
