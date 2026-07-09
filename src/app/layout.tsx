c6a8e563bcf2f080fcc488475b39a41381409af9
junyeoplee <jyeop920@gmail.com>
2026-07-09 21:46:07 +0900

    fix: 배포 빌드 실패 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import { Navigation } from "@widgets/navigation";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "뉴브랜치",
@@ -26,7 +15,7 @@ const RootLayout = async ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>
         <Navigation />
         {children}
